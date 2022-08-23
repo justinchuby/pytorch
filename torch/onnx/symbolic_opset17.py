2662267179b559a860857cc3228940cbd37b98c6
Justin Chu <justinchu@microsoft.com>
2022-08-23 22:18:24 +0000

    [ONNX] Opset 17 STFT support
    ghstack-source-id: be5336e569ba3577994bc0f323a7502cc7c6b2d0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/83944

--- torch/onnx/symbolic_opset17.py
+++ torch/onnx/symbolic_opset17.py
@@ -16,16 +16,18 @@ New operators:
 """
 
 import functools
+import math
 from typing import Sequence
 
 from torch import _C
-from torch.onnx import symbolic_helper
+import torch
+from torch.onnx import symbolic_helper, _type_utils
 from torch.onnx._internal import jit_utils, registration
 
 # EDITING THIS FILE? READ THIS FIRST!
 # see Note [Edit Symbolic Files] in README.md
 
-__all__ = ["layer_norm"]
+__all__ = ["layer_norm", "stft"]
 
 _onnx_symbolic = functools.partial(registration.onnx_symbolic, opset=17)
 
@@ -54,3 +56,41 @@ def layer_norm(
         epsilon_f=eps,
         axis_i=axis,
     )
+
+
+@_onnx_symbolic("aten::stft")
+@symbolic_helper.parse_args("v", "i", "i", "i", "v", "b", "b", "b", "none")
+def stft(
+    g,
+    self: _C.Value,
+    n_fft: int,
+    hop_length: int,
+    win_length: int,
+    window: _C.Value,
+    normalized: bool,
+    onesided: bool,
+    return_complex: bool,
+) -> _C.Value:
+    # return_complex is always True since version 1.8.0
+    # n_fft: Size of the FFT. frame_length in ONNX
+    # hop_length: Stride of the FFT. frame_step in ONNX
+    # window: Window to use in FFT. window in ONNX
+    input_dtype = _type_utils.JitScalarType.from_name(self.type().scalarType()).dtype()
+    frame_step = g.op("Constant", value_t=torch.tensor(hop_length, dtype=torch.int64))
+    frame_length = g.op("Constant", value_t=torch.tensor(n_fft, dtype=torch.int64))
+    stft_result = g.op(
+        "STFT",
+        self,
+        frame_step,
+        window,
+        frame_length,
+        onesided_i=onesided,
+    )
+    # Reconstruct the reel output to complex output
+    # TODO
+    # Normalization multiplies the result by 1 / sqrt(frame_length)
+    if normalized:
+        sqrt_frame_length = torch.tensor(math.sqrt(n_fft), dtype=input_dtype)
+        return g.op("Div", stft_result, g.op("Constant", value_t=sqrt_frame_length))
+
+    return stft_result
